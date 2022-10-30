22d545bc4a363cf61178bbab81032ca639872033
Vova Shevchuk <volodymyr.shevchuk.ua@gmail.com>
2022-10-30 20:17:48 +0200

    ready to deploy

--- src/test/java/secondweek/LongTableCreatorTest.java
+++ src/test/java/secondweek/LongTableCreatorTest.java
@@ -1,10 +1,9 @@
 package secondweek;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class LongTableCreatorTest {
 
